c257d099adc8402af636a82def5ddd4303982bd2
Kyle Hagler <khagler.kh@gmail.com>
2020-07-17 09:25:06 -0500

    added sidebars to about page

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from "react-router";
 
 import './AboutPage.css'
 
 //This page is for anyone to read (i.e. don't need to be logged in to see it)
-const AboutPage = () => (
-  <div>
-    <center>
-      <div className='card' id='aboutContent' style={{ width: '60%' }}>
-        <br />
+const AboutPage = (props) => (
+  <div className='page' style={{display:'flex', minHeight:'94%', top:'38px', textAlign:'center', minWidth:'100%'}}>
+    <div className='col-sm' style={{ backgroundColor: props.color.outlineColor, float: 'left', maxWidth: '5%' }}></div>
+  
+      <div className='card' id='aboutContent' style={{ width: '80%' }}>
+        <br /><br/><br/><br/>
         <h1>So why journal?</h1><br />
-        <h3 style={{ width: '80%', margin: 'auto' }}>
+        <h3 style={{ width: '50%', marginLeft:'auto', marginRight:'auto' }}>
           Journaling has been found to help in a multitude of ways! These include but are not limited to:</h3>
         <center>
-          <div style={{ margin: '3%', textAlign: 'left', width: '40%' }}>
+          <div style={{ margin: '1%', textAlign: 'left', width: '40%' }}>
             <li>Helps establish future goals</li>
             <li>Helps one reflect and solve problems</li>
             <li>Allows opportunity for self-dialogue and self-expression</li>
@@ -21,10 +24,16 @@ const AboutPage = () => (
             <li>Able to reflect on past and see personal growth</li>
           </div>
         </center>
-
       </div>
-    </center>
+
+    
+    <div className='col-sm' style={{ backgroundColor: props.color.outlineColor, float: 'right', maxWidth: '5%' }}></div>
   </div>
 );
 
-export default AboutPage;
+
+const mapStateToProps = state => ({
+  color: state.color
+});
+
+export default withRouter(connect(mapStateToProps)(AboutPage));
